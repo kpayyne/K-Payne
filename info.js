abf3a4f25dc4f01f10c1c2cf17ceef34692c007a
Kalem Williams <kmw1@graceland.edu>
2018-03-09 12:44:22 -0600

    website update

---
+++ info.js
@@ -0,0 +1,9 @@
+
+        
+		    var name = "Your Name Here";
+		
+            document.write("<span id ='footer'>Kalem's Website</span><br />");
+			document.write("<span id ='footer'>© 2018 Kalem Williams</span><br />");
+			document.write("<span id ='footer'>Modified Mar 5 2018</span><br />");
+			document.write("<span id ='footer'>Retrieved: Fri Mar 09 2018 12:42 (Central Time Zone)</span><br />");
+
